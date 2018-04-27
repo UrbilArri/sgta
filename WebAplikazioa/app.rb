f40da797423ac14765bc5d0ea8b3e5cfdf3a845e
UrbilArri <2garribillagaurbil@gmail.com>
2018-04-27 16:56:16 +0200

    diseinua garatu

--- WebAplikazioa/app.rb
+++ WebAplikazioa/app.rb
@@ -2,47 +2,22 @@ require 'sinatra'
 require_relative 'models/product.rb'
 
 get '/' do
+  #HASIERAKO ORRIA
   @page_title = "Hasierako orria"
-  render layout: "hasierakoLayout"
-  erb :index
+  @orria = File.read('views/index.erb')
+  erb :hasierakoLayout
 end
 
 get '/erregistratu' do
   #ERREGISTRATZEKO ORRIA ERAKUSTEN DA HEMEN
   @page_title = "Erregistroa"
-  layout :layout
-  erb :erregistratu
+  @orria = File.read('views/erregistratu.erb')
+  erb :hasierakoLayout
 end
 
-get '/team' do
-  # TEAM PAGE LISTING THE TEAM MEMBERS
-  @page_title = "The Team"
-  erb :team
+get '/login' do
+  #LOGEATZEKO ORRIA
+  @page_title = "Logeatu"
+  @orria = File.read('views/login.erb')
+  erb :hasierakoLayout
 end
-
-get '/products' do
-  # PRODUCTS PAGE LISTING ALL THE PRODUCTS
-  @page_title = "All Products"
-  @products = Product.sample_locations
-  erb :products
-end
-
-get '/products/location/:location' do
-  # PAGE DISPLAYING ALL PHOTOS FROM ONE LOCATION
-  @products = Product.find_by_location(params[:location])
-  @page_title =params[:location]
-  erb :category
-end
-
-get '/products/:id' do
-  # PAGE DISPLAYING ONE PRODUCT WITH A GIVEN ID
-  @product = Product.find(params[:id].to_i)
-  @page_title = @product.title
-  erb :single
-end
-
-get '/deals' do
-  # PAGE LISTING ALL PRODUCTS UNDER $10
-  @products = Product.find_deals
-  erb :deals
- end
