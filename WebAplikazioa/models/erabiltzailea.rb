3500035999cb994507e28488a0f062e77d1999b3
UrbilArri <2garribillagaurbil@gmail.com>
2018-05-15 23:56:03 +0200

    erregistratzea bukaturik

--- WebAplikazioa/models/erabiltzailea.rb
+++ WebAplikazioa/models/erabiltzailea.rb
@@ -14,28 +14,32 @@ class Erabiltzailea
 	validates :hiria, format: {with: /[A-Za-z]{0,25}/, message: "hiriaren formatu desegokia"}
 	validates :erabIzena, format: {with: /[A-Za-z]{1,15}/, message: "erabiltzaile izenaren formatu desegokia"}#, uniqueness: true
 	validates :pasahitza, format: {with: /[A-Za-z0-9]{4,25}/, message: "pasahitzak gutxienez lau luzerakoa"}
-	validate :pasahitzak_berdinak
+	validate :pasahitzak_berdinak, :bakarra
 
 	def pasahitzak_berdinak
 		errors.add(:pasahitza2, "pasahitzak ez dira berdinak") if (pasahitza != pasahitza2)
 	end
 
-	def bakarra
-	   File.open('/home/urbil/Escritorio/rubyProiektua/WebAplikazioa/data/erabiltzailea.json', "r+") do |f|
-	  	hash = JSON.parse(f.read)
-	  	puts hash["erabiltzaileak"]["izena"]
-	  end
-	end
+	DATA = File.read('/home/urbil/Escritorio/rubyProiektua/WebAplikazioa/data/erabiltzailea.json')
 
 	#def initialize(erabiltzaile_data)
-	  #@id = erabiltzaile_data[:id]
-	  #@izena = erabiltzaile_data[:izena]
-	  #@abizena = erabiltzaile_data[:abizena]
-	  #@korreoa = erabiltzaile_data[:korreoa]
-	  #@hiria = erabiltzaile_data[:hiria]
-	  #@erabIzena = erabiltzaile_data[:erabIzena]
-	  #@pasahitza = erabiltzaile_data[:pasahitza]
+	  #@id = erabiltzaile_data['id']
+	  #@izena = erabiltzaile_data['izena']
+	  #@abizena = erabiltzaile_data['abizena']
+	  #@korreoa = erabiltzaile_data['korreoa']
+	  #@hiria = erabiltzaile_data['hiria']
+	  #@erabIzena = erabiltzaile_data['erabIzena']
+	  #@pasahitza = erabiltzaile_data['pasahitza']
 	#end
+
+	def bakarra
+		hash = JSON.parse(DATA)
+		@erabiltzaileak = hash["erabiltzaileak"]
+		@erabIzenak = @erabiltzaileak.collect{| erab | erab['izena'] = erabIzena}
+		@korreoak = @erabiltzaileak.collect{| erab | erab['korreoa'] = korreoa}
+		errors.add(:erabIzena, "erabiltzaile izen hori jada hartuta dago") if @erabIzenak.length > 0
+		errors.add(:korreoa, "korreo hori jada hartuta dago") if @korreoak.length > 0
+	end
 	
 	# Pertsona objektuen Array-a itzuli
 	def self.all
